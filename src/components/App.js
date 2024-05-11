94321ee5948ff22aba66a82a349c1fc758ef33df
bearatron <84403830+bearatron@users.noreply.github.com>
2024-05-10 21:38:54 -0400

    Move to components folder

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "components/Header";
+import GameLogic from "components/GameLogic";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <GameLogic />
+    </div>
+  );
+}
+
+export default App;
