93fce35f6c4912a86063a16c2ef58301fbef483d
bearatron <84403830+bearatron@users.noreply.github.com>
2024-08-16 12:31:26 -0400

    Added limit of 6 guesses

--- src/components/GuessItem.js
+++ src/components/GuessItem.js
@@ -1,8 +1,10 @@
 import DirectionImage from "./DirectionImage";
 import "./GuessItem.css";
 
-function GuessItem({ name, distance, direction, percentage }) {
-  return (
+function GuessItem({ name, distance, direction, percentage, empty = false }) {
+  return empty ? (
+    <div className="guess-item-empty"></div>
+  ) : (
     <div className="guess-item">
       <div className="guess-item__name">{name}</div>
       <div className="guess-item__distance">{distance}</div>
