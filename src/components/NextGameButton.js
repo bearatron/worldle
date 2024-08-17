3ea365116b3530120f3c0113b683fb2df4b2b1c1
bearatron <84403830+bearatron@users.noreply.github.com>
2024-08-17 13:32:19 -0400

    Add styling

--- src/components/NextGameButton.js
+++ src/components/NextGameButton.js
@@ -1,5 +1,11 @@
+import "./NextGameButton.css";
+
 function NextGameButton({ resetGame }) {
-  return <button onClick={resetGame}>Next Game</button>;
+  return (
+    <button className="next-game-button" onClick={resetGame}>
+      Next Game
+    </button>
+  );
 }
 
 export default NextGameButton;
