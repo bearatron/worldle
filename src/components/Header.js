5e9c404d5f301f0d0b6a0d4a35426a1a315a2920
bearatron <84403830+bearatron@users.noreply.github.com>
2024-08-17 22:10:32 -0400

    Add styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
+import "./Header.css";
+
 function Header() {
   return (
-    <>
+    <div className="header">
+      <img className="globe-img" src="./globe-showing-americas.png" />
       <h1>Worldle</h1>
-    </>
+    </div>
   );
 }
 
