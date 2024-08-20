a9269d9b78ac3dd2841c38c1350fe517541123ba
bearatron <84403830+bearatron@users.noreply.github.com>
2024-08-19 20:25:38 -0400

    Add globe emoji

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,11 @@ import "./Header.css";
 function Header() {
   return (
     <div className="header">
-      <img className="globe-img" src="./globe-showing-americas.png" />
+      <img
+        className="globe-img"
+        src="./globe-showing-americas.png"
+        alt="globe showing americas"
+      />
       <h1>Worldle</h1>
     </div>
   );
